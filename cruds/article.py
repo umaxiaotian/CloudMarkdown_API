17c11828948356a80d6c166d9b477c6cde2a4484
小畑佑馬 <yuumaobata1008@outlook.com>
2022-04-12 02:09:35 +0900

    DESC大きい順に並べるように変更

--- cruds/article.py
+++ cruds/article.py
@@ -4,8 +4,7 @@ def getArticleList():
     # 記事情報を取得
     query = Article.select().get()
     article = []
-    for user in query.select().order_by(Article.good_count).limit(10):
+    for user in query.select().order_by(Article.good_count.desc()).limit(10):
         article.append(user)
-
 
     return article
