aa3ce40e9b2681bdb6b88dbc877d03643cb93062
小畑佑馬 <yuumaobata1008@outlook.com>
2022-04-14 02:14:59 +0900

    no message

--- cruds/article.py
+++ cruds/article.py
@@ -3,6 +3,7 @@ from ast import dump
 from multiprocessing.dummy import Array
 from click import echo
 from models.article import Article
+from models.user import User
 from cruds.auth import *
 
 
@@ -11,8 +12,9 @@ def getArticleList():
     query = Article.select().get()
     articles = []
     for article in query.select().order_by(Article.good_count.desc()).limit(10):
+        relate_user_name = User.get(article.relate_user_id).name
         articles.append(
-            {"id": article.id, "title": article.title, "detail": article.detail, "tags": article.tags, "post_date": article.post_date})
+            {"relate_user_id": article.relate_user_id, "relate_user_name": relate_user_name, "title": article.title, "detail": article.detail, "good_count": article.good_count, "tags": article.tags, "post_date": article.post_date})
 
     return articles
 
@@ -22,6 +24,6 @@ def getMyArticleList(user_id: User = Depends(get_current_user)):
     articles = ["list"]
     for article in query.select().where(Article.relate_user_id == user_id).order_by(Article.post_date.desc()):
         articles.append(
-            {"id": article.id, "title": article.title, "detail": article.detail, "tags": article.tags, "post_date": article.post_date})
+            {"relate_user": article.relate_user_id, "title": article.title, "detail": article.detail, "good_count": article.good_count, "tags": article.tags, "post_date": article.post_date})
 
     return articles
